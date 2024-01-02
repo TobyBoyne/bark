3ca8a790e582418122508739f5324b72896f5f6e
TobyBoyne <tobyboyne@gmail.com>
2024-01-02 16:20:43 +0000

    Remove `Leaf`, refactor as attribute of `Node`

--- alfalfa/leaf_gp/gbm_model.py
+++ alfalfa/leaf_gp/gbm_model.py
@@ -329,10 +329,10 @@ class GbmNode(GbmType):
 
         #TODO: check whether split_code_pred has any impact
         child = node.left
-        if isinstance(child, Leaf):
+        if child.is_leaf:
             self.left = LeafNode(
                 split_code_pred = 0.0,
-                leaf_id = child.leaf_id
+                leaf_id = child.node_id
             )
         else:
             self.left = GbmNode(
@@ -343,10 +343,10 @@ class GbmNode(GbmType):
 
         # read right node
         child = node.right
-        if isinstance(child, Leaf):
+        if child.is_leaf:
             self.right = LeafNode(
                 split_code_pred = 0.0,
-                leaf_id = child.leaf_id
+                leaf_id = child.node_id
             )
         else:
             self.right = GbmNode(
--- tests/test_tree_structure.py
+++ tests/test_tree_structure.py
@@ -1,13 +1,14 @@
 from alfalfa.tree_models.forest import AlfalfaTree, Node, Leaf
 
 def test_tree_structure_equals():
-    tree1 = Node(var_idx=0, threshold=0.5)
+    tree1 = Node(var_idx=0, threshold=0.5, left=Leaf(), right=Leaf())
 
-    tree2 = Node(var_idx=0, threshold=0.5)
+    tree2 = Node(var_idx=0, threshold=0.5, left=Leaf(), right=Leaf())
 
     tree3 = Node(
         var_idx=0, threshold=0.5,
-        right=Node(var_idx=1, threshold=1.0)
+        left=Leaf(),
+        right=Node(var_idx=1, threshold=1.0, left=Leaf(), right=Leaf())
     )
 
     assert tree1.structure_eq(tree2)
