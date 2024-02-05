007a2b02c5df600408b95d6c10a9652bee86e050
TobyBoyne <tobyboyne@gmail.com>
2024-02-05 14:55:07 +0000

    Add tests for tree transitions

--- tests/bart/test_tree_transition.py
+++ tests/bart/test_tree_transition.py
@@ -1,4 +1,8 @@
-from alfalfa.fitting.bart.tree_transitions import GrowTransition
+from alfalfa.fitting.bart.tree_transitions import (
+    ChangeTransition,
+    GrowTransition,
+    PruneTransition,
+)
 from alfalfa.forest import AlfalfaTree, DecisionNode
 
 
@@ -13,3 +17,31 @@ def test_grow_transition():
 
     transition.apply()
     assert tree.structure_eq(AlfalfaTree(root=DecisionNode(left=DecisionNode())))
+
+
+def test_prune_transition():
+    tree = AlfalfaTree(root=DecisionNode(left=DecisionNode()))
+    transition = PruneTransition(tree, tree.root.left)
+    with transition:
+        assert tree.structure_eq(AlfalfaTree(root=DecisionNode()))
+
+    # context manager correctly applies inverse transition upon exit
+    assert tree.structure_eq(AlfalfaTree(root=DecisionNode(left=DecisionNode())))
+
+    transition.apply()
+    assert tree.structure_eq(AlfalfaTree(root=DecisionNode()))
+
+
+def test_change_transition():
+    tree = AlfalfaTree(root=DecisionNode(var_idx=0, threshold=0.5))
+    transition = ChangeTransition(tree, tree.root, 1, 2.0)
+    with transition:
+        assert tree.structure_eq(
+            AlfalfaTree(root=DecisionNode(var_idx=1, threshold=2.0))
+        )
+
+    # context manager correctly applies inverse transition upon exit
+    assert tree.structure_eq(AlfalfaTree(root=DecisionNode(var_idx=0, threshold=0.5)))
+
+    transition.apply()
+    assert tree.structure_eq(AlfalfaTree(root=DecisionNode(var_idx=1, threshold=2.0)))
