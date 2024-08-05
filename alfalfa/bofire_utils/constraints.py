51d3acc2073ec3f35943048f678c672c2b30121d
Toby Boyne <tobyboyne@gmail.com>
2024-08-05 16:52:21 +0100

    First attempt at automatically making constraints from sympy

--- alfalfa/bofire_utils/constraints.py
+++ alfalfa/bofire_utils/constraints.py
@@ -1,8 +1,10 @@
-from typing import Annotated, Callable, List, Literal, Tuple
+from collections.abc import Callable
+from typing import Annotated, List, Literal, Tuple
 
 import gurobipy
 import numpy as np
 import pandas as pd
+import sympy
 from bofire.data_models.constraints.api import (
     AnyConstraint,
     EqalityConstraint,
@@ -11,8 +13,13 @@ from bofire.data_models.constraints.api import (
     LinearConstraint,
     NonlinearConstraint,
 )
+from bofire.data_models.domain.api import Domain
 from bofire.data_models.types import make_unique_validator
 from pydantic import AfterValidator, Field, model_validator
+from sympy.core.relational import Equality, LessThan
+
+EvaluatedSymbolT = gurobipy.GenExpr | gurobipy.Var | gurobipy.LinExpr | float
+
 
 QuadraticFeatureKeys = Annotated[
     List[Tuple[str, str]],
@@ -141,3 +148,78 @@ def apply_constraint_to_model(
         return model_core.addConstr(gur_constr)
 
     raise NotImplementedError(f"{constraint.type} is not yet supported.")
+
+
+def _get_var_or_create(
+    model_core: gurobipy.Model,
+    args: list[EvaluatedSymbolT],
+    name_format: str,
+    escape_float=False,
+    **var_kwargs,
+) -> gurobipy.Var:
+    """Get or create a variable in the model core based on the arguments.
+    Returns None if no auxilliary variable is needed."""
+    if escape_float and any(isinstance(arg, (float, sympy.Float)) for arg in args):
+        return None
+    args_names = [v.VarName if isinstance(v, gurobipy.Var) else str(v) for v in args]
+    name = name_format.format(*args_names)
+    gur_var = model_core.getVarByName(name_format)
+    if gur_var is None:
+        gur_var = model_core.addVar(name=name, **var_kwargs)
+    return gur_var
+
+
+def _recurse_sympy(
+    expr: sympy.Expr, model_core: gurobipy.Model
+) -> gurobipy.GenExpr | gurobipy.Var | gurobipy.LinExpr | float:
+    if expr.is_Add:
+        return gurobipy.quicksum(_recurse_sympy(arg, model_core) for arg in expr.args)
+    elif expr.is_Mul:
+        args = [_recurse_sympy(arg, model_core) for arg in expr.args]
+
+        arg_prod = _get_var_or_create(
+            model_core, args, name_format="_({}*{})", escape_float=True
+        )
+        if arg_prod is None:
+            return args[0] * args[1]
+        model_core.addConstr(arg_prod == args[0] * args[1])
+        model_core.update()
+        if len(expr.args) == 2:
+            return arg_prod
+        rest_prod = _recurse_sympy(sympy.Mul(*expr.args[2:]), model_core)
+        all_prod = _get_var_or_create(
+            model_core, [arg_prod, rest_prod], name_format="_({}*{})"
+        )
+        model_core.addConstr(all_prod == arg_prod * rest_prod)
+        model_core.update()
+
+        return all_prod
+    elif expr.is_Number:
+        return expr.evalf()
+    elif expr.is_Symbol:
+        return model_core.getVarByName(expr.name)
+    elif expr.is_Pow:
+        args = [_recurse_sympy(arg, model_core) for arg in expr.args]
+        arg_exp = _get_var_or_create(model_core, args, name_format="_{}^{}")
+        model_core.addGenConstrPow(xvar=args[0], yvar=arg_exp, a=args[1])
+        model_core.update()
+        return arg_exp
+    else:
+        raise NotImplementedError(f"{expr} is not yet supported.")
+
+
+def add_constr_expr_to_gurobipy_model(
+    expression_string: str, model_core: gurobipy.Model, domain: Domain
+):
+    features = domain.inputs.get_keys()
+    symbols = sympy.symbols(features)
+    key_to_symbol = {key: symbol for key, symbol in zip(features, symbols)}
+
+    expr = sympy.parse_expr(expression_string, key_to_symbol)
+    assert isinstance(
+        expr, (LessThan, Equality)
+    ), f"{expr.func} is not a relational expression."
+    lhs, rhs = expr.args
+    gur_expr = _recurse_sympy(lhs, model_core)
+    gur_constr = gur_expr <= rhs if isinstance(expr.func, LessThan) else gur_expr == rhs
+    model_core.addConstr(gur_constr)
