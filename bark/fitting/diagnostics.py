04f32f9b72fb1b2af8500d346cdcdb66d577fee6
Toby Boyne <tobyboyne@gmail.com>
2024-10-23 17:42:41 +0100

    Plot mutual information against samples

--- bark/fitting/diagnostics.py
+++ bark/fitting/diagnostics.py
@@ -1,15 +1,18 @@
 import numpy as np
+from numba import njit
 from scipy.optimize import linear_sum_assignment
 
 from bark.fitting.tree_traversal import get_node_subspace, terminal_nodes
 
 
+@njit
 def _subspace_intersection_volume(sub_u, sub_v):
     lb = np.maximum(sub_u[:, 0], sub_v[:, 0])
     ub = np.minimum(sub_u[:, 1], sub_v[:, 1])
     return np.prod(np.maximum(0, ub - lb))
 
 
+@njit
 def _subspace_intersection_logvolume(sub_u, sub_v):
     lb = np.maximum(sub_u[:, 0], sub_v[:, 0])
     ub = np.minimum(sub_u[:, 1], sub_v[:, 1])
@@ -20,12 +23,14 @@ def _subspace_intersection_logvolume(sub_u, sub_v):
         return np.sum(np.log(np.maximum(0, ub - lb)))
 
 
+@njit
 def _subspace_product_logvolume(sub_u, sub_v):
     logvolume_u = np.sum(np.log(sub_u[:, 1] - sub_u[:, 0]))
     logvolume_v = np.sum(np.log(sub_v[:, 1] - sub_v[:, 0]))
     return logvolume_u + logvolume_v
 
 
+@njit
 def mutual_information_tree_pair(tree_u, tree_v, bounds, feat_types):
     volume_domain = np.prod(bounds[:, 1] - bounds[:, 0])
     leaves_u, leaves_v = map(terminal_nodes, (tree_u, tree_v))
@@ -54,5 +59,5 @@ def mutual_information_forest_pair(forest_u, forest_v, bounds, feat_types):
                 tree_u, tree_v, bounds, feat_types
             )
 
-    row_ind, col_ind = linear_sum_assignment(mutual_info_matrix)
+    row_ind, col_ind = linear_sum_assignment(mutual_info_matrix, maximize=True)
     return mutual_info_matrix[row_ind, col_ind].sum()
--- examples/mcmc/mutual_information.py
+++ examples/mcmc/mutual_information.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 from bark.benchmarks import StyblinskiTang
@@ -5,7 +6,7 @@ from bark.bofire_utils.domain import get_feature_bounds, get_feature_types_array
 from bark.fitting.diagnostics import mutual_information_forest_pair
 
 with open(r"C:\Users\tobyb\phd\bark\forest.npy", "rb") as f:
-    forest = np.load(f)
+    forest_samples = np.load(f)
 
 domain = StyblinskiTang().domain
 bounds = np.array(
@@ -13,19 +14,12 @@ bounds = np.array(
 )
 feat_types = get_feature_types_array(domain)
 
-forest_samples = forest.reshape(-1, *forest.shape[-2:])
-
-mutual_information_matrix = np.zeros((forest_samples.shape[0], forest_samples.shape[0]))
-# for i in range(forest_samples.shape[0]):
-#     for j in range(i, forest_samples.shape[0]):
-#         mutual_information_matrix[i, j] = mutual_information(forest_samples[i, 0], forest_samples[j, 0], bounds, feat_types)
-mi_01 = mutual_information_forest_pair(
-    forest_samples[0], forest_samples[1], bounds, feat_types
-)
-mi_00 = mutual_information_forest_pair(
-    forest_samples[0], forest_samples[0], bounds, feat_types
-)
-print(mi_01, mi_00)
-
-# plt.imshow(mutual_information_matrix)
-# plt.show()
+mutual_information_matrix = np.zeros((forest_samples.shape[0], forest_samples.shape[1]))
+for i in range(forest_samples.shape[0]):
+    for j in range(forest_samples.shape[1]):
+        mutual_information_matrix[i, j] = mutual_information_forest_pair(
+            forest_samples[0, 0], forest_samples[i, j], bounds, feat_types
+        )
+print(mutual_information_matrix[0, 0])
+plt.imshow(mutual_information_matrix)
+plt.show()
