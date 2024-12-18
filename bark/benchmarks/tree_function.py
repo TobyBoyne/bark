17ffd142a71accb4026ed781b426a81f5bf23111
Toby Boyne <tobyboyne@gmail.com>
2024-12-18 18:02:16 +0000

    Update tree function benchmark

--- bark/benchmarks/tree_function.py
+++ bark/benchmarks/tree_function.py
@@ -9,18 +9,20 @@ from bofire.data_models.features.api import (
 )
 from bofire.data_models.objectives.api import MinimizeObjective
 
-from bark.fitting.tree_proposals import NODE_PROPOSAL_DTYPE, grow
-from bark.forest import FeatureTypeEnum, create_empty_forest, pass_through_forest
+from bark.bofire_utils.domain import get_feature_types_array
+from bark.fitting.tree_proposals import NodeProposal, grow
+from bark.forest import create_empty_forest, pass_through_forest
 
 
-def sample_tree_function_from_structure(forest):
+def sample_tree_function_from_structure(
+    forest: np.ndarray, domain: Domain, tree_rng: np.random.Generator
+):
     """Sample a tree function $f(x)=\sum_j g(x; T_j)$"""
 
-    rng_leaf_value = np.random.default_rng(seed=0)
-    leaf_values = rng_leaf_value.standard_normal(forest.shape)
+    leaf_values = tree_rng.standard_normal(forest.shape)
+    feat_types = get_feature_types_array(domain)
 
     def f(x):
-        feat_types = np.full(x.shape[1], FeatureTypeEnum.Cont.value)
         leaves = pass_through_forest(forest, x, feat_types)
         g_leaf_values = leaf_values[np.arange(leaf_values.shape[0]), leaves]
         f_value = g_leaf_values.sum(axis=1)
@@ -29,12 +31,11 @@ def sample_tree_function_from_structure(forest):
     return f
 
 
-def sample_tree_structure_from_prior(m: int, domain: Domain):
+def sample_tree_structure_from_prior(m: int, domain: Domain, rng: np.random.Generator):
     forest = create_empty_forest(m)
     alpha = 0.95
     beta = 2.0
     dim = len(domain.inputs)
-    rng = np.random.default_rng(seed=0)
     for tree in forest:
         new_nodes = [0]
         while new_nodes:
@@ -43,10 +44,10 @@ def sample_tree_structure_from_prior(m: int, domain: Domain):
             depth_prior = alpha * (1 + depth) ** (-beta)
             if rng.uniform() > depth_prior:
                 continue
-            node_proposal = np.zeros((1,), dtype=NODE_PROPOSAL_DTYPE)[0]
-            node_proposal["node_idx"] = node_idx
-            node_proposal["new_feature_idx"] = rng.integers(dim)
-            node_proposal["new_threshold"] = rng.uniform(0, 1)
+            node_proposal = NodeProposal()
+            node_proposal.node_idx = node_idx
+            node_proposal.new_feature_idx = rng.integers(dim)
+            node_proposal.new_threshold = rng.uniform(0, 1)
 
             grow(tree, node_proposal)
             new_nodes.extend([tree[node_idx]["left"], tree[node_idx]["right"]])
@@ -59,7 +60,7 @@ class TreeFunction(Benchmark):
 
     This is a good test that BARK is indeed able to optimize on tree functions."""
 
-    def __init__(self, dim=5, m=50, **kwargs):
+    def __init__(self, dim=5, m=50, function_seed=1, **kwargs):
         super().__init__(**kwargs)
         self._domain = Domain(
             inputs=Inputs(
@@ -75,8 +76,9 @@ class TreeFunction(Benchmark):
             ),
         )
 
-        forest = sample_tree_structure_from_prior(m, self._domain)
-        self._tree_func = sample_tree_function_from_structure(forest)
+        rng = np.random.default_rng(function_seed)
+        forest = sample_tree_structure_from_prior(m, self._domain, rng)
+        self._tree_func = sample_tree_function_from_structure(forest, self._domain, rng)
 
     def _f(self, X: pd.DataFrame, **kwargs) -> pd.DataFrame:
         ys = self._tree_func(X.to_numpy())
@@ -86,12 +88,13 @@ class TreeFunction(Benchmark):
 if __name__ == "__main__":
     benchmark = TreeFunction(dim=1, m=10)
     x = pd.DataFrame(
-        data=np.linspace(0, 1, 100)[:, None], columns=benchmark.domain.inputs.get_keys()
+        data=np.linspace(0, 1, 1000)[:, None],
+        columns=benchmark.domain.inputs.get_keys(),
     )
     y = benchmark.f(x)
     plt.step(x, y)
     x = pd.DataFrame(
-        data=np.linspace(0.01, 1.02, 100)[:, None],
+        data=np.linspace(0.01, 1.02, 1000)[:, None],
         columns=benchmark.domain.inputs.get_keys(),
     )
     y = benchmark.f(x)
