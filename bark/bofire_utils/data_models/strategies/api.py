6eb856ac376469aadc8923eeb132375f467f5d46
TobyBoyne <tobyboyne@gmail.com>
2025-01-06 12:22:51 +0000

    SMAC strategy now working

--- bark/bofire_utils/data_models/strategies/api.py
+++ bark/bofire_utils/data_models/strategies/api.py
@@ -1,3 +1,4 @@
+from bark.bofire_utils.data_models.strategies.smac import SMACStrategy  # noqa: F401
 from bark.bofire_utils.data_models.strategies.tree_kernel import (
     TreeKernelStrategy,  # noqa: F401
 )
--- bark/bofire_utils/data_models/strategies/smac.py
+++ bark/bofire_utils/data_models/strategies/smac.py
@@ -1,5 +1,48 @@
-from bofire.data_models.strategies.api import Strategy
+from typing import Literal, Type
+
+from bofire.data_models.constraints.api import (
+    Constraint,
+    LinearEqualityConstraint,
+    LinearInequalityConstraint,
+    NChooseKConstraint,
+)
+from bofire.data_models.features.api import (
+    CategoricalDescriptorInput,
+    CategoricalInput,
+    ContinuousInput,
+    ContinuousOutput,
+    DiscreteInput,
+    Feature,
+)
+from bofire.data_models.objectives.api import (
+    MaximizeObjective,
+    MinimizeObjective,
+    Objective,
+)
+from bofire.data_models.strategies.predictives.predictive import PredictiveStrategy
 
 
-class SMACStrategy(Strategy):
-    pass
+class SMACStrategy(PredictiveStrategy):
+    type: Literal["SMACStrategy"] = "SMACStrategy"
+
+    @classmethod
+    def is_constraint_implemented(cls, my_type: Type[Constraint]) -> bool:
+        return my_type in [
+            LinearEqualityConstraint,
+            LinearInequalityConstraint,
+            NChooseKConstraint,
+        ]
+
+    @classmethod
+    def is_feature_implemented(cls, my_type: Type[Feature]) -> bool:
+        return my_type in [
+            CategoricalInput,
+            DiscreteInput,
+            CategoricalDescriptorInput,
+            ContinuousInput,
+            ContinuousOutput,
+        ]
+
+    @classmethod
+    def is_objective_implemented(cls, my_type: Type[Objective]) -> bool:
+        return my_type in [MinimizeObjective, MaximizeObjective]
