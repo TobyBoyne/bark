7f92c8c288e0695302ef6c5a87dbdc0489a7ce2f
Toby Boyne <tobyboyne@gmail.com>
2025-05-28 17:47:07 +0100

    Prevent OOM with large combinatorial spaces

--- bark/bofire_utils/strategies/acqf_optimization.py
+++ bark/bofire_utils/strategies/acqf_optimization.py
@@ -1,9 +1,11 @@
 """Mixed acquisition function optimization"""
 
-from typing import Callable, Dict, List, Optional, Tuple
+from typing import List, Optional, Tuple
 
+import pandas as pd
 from bofire.data_models.domain.api import Domain
 from bofire.data_models.features.api import DiscreteInput
+from bofire.data_models.types import InputTransformSpecs
 from bofire.strategies.predictives.acqf_optimization import BotorchOptimizer
 from botorch.acquisition import AcquisitionFunction
 from botorch.optim import optimize_acqf_mixed_alternating
@@ -11,24 +13,20 @@ from torch import Tensor
 
 
 class AlternatingBotorchOptimizer(BotorchOptimizer):
-    def _optimize_acqf_continuous(
+    def _optimize(
         self,
-        domain: Domain,
         candidate_count: int,
         acqfs: List[AcquisitionFunction],
-        bounds: Tensor,
-        ic_generator: Callable,
-        ic_gen_kwargs: Dict,
-        nonlinear_constraints: List[Callable[[Tensor], float]],
-        fixed_features: Optional[Dict[int, float]],
-        fixed_features_list: Optional[List[Dict[int, float]]],
-        sequential: bool = False,
+        domain: Domain,
+        input_preprocessing_specs: InputTransformSpecs,
+        experiments: Optional[pd.DataFrame] = None,
     ) -> Tuple[Tensor, Tensor]:
-        # this is not correct, as specs will matter if eg. there is a one-hot encoding
-        specs = {}
+        bounds = self.get_bounds(domain, input_preprocessing_specs)
         discrete_keys = domain.inputs.get_keys(includes=DiscreteInput)
-        discrete_dims = domain.inputs.get_feature_indices(specs, discrete_keys)
-        return optimize_acqf_mixed_alternating(
+        discrete_dims = domain.inputs.get_feature_indices(
+            input_preprocessing_specs, discrete_keys
+        )
+        candidates, _ = optimize_acqf_mixed_alternating(
             acq_function=acqfs[0],
             bounds=bounds,
             q=candidate_count,
@@ -36,3 +34,9 @@ class AlternatingBotorchOptimizer(BotorchOptimizer):
             raw_samples=self.n_raw_samples,
             discrete_dims=discrete_dims,
         )
+
+        candidates = self._candidates_tensor_to_dataframe(
+            candidates, domain, input_preprocessing_specs
+        )
+
+        return candidates
